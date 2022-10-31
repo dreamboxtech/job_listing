ece5d148e62c48c1b7f46fdb31c419c8e3125d25
dreamboxtech <bbbolaleye@gmail.com>
2022-10-31 18:00:15 +0100

    Factory Listing Seeder trial -- worked perfectly

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,27 +15,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
-        Listing::create([
-            'title' => 'Laravel Senior Developer',
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'email1@email.com',
-            'website' => 'https://www.acme.com',
-            'description' => 'Here is where you can register web routes for your application. These
-            | routes are loaded by the RouteServiceProvider within a group which
-            | contains the "web" middleware group. Now create something great!'
-            ]);
-        Listing::create([
-            'title' => 'Python Senior Developer',
-            'tags' => 'python, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Georgia, LA',
-            'email' => 'email2@email.com',
-            'website' => 'https://www.py.acme.com',
-            'description' => 'Here is where you can register web routes for your application. These
-            | routes are loaded by the RouteServiceProvider within a group which
-            | contains the "web" middleware group. Now create something great!'
-        ]);
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Here is where you can register web routes for your application. These
+        //     | routes are loaded by the RouteServiceProvider within a group which
+        //     | contains the "web" middleware group. Now create something great!'
+        //     ]);
+        // Listing::create([
+        //     'title' => 'Python Senior Developer',
+        //     'tags' => 'python, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Georgia, LA',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'https://www.py.acme.com',
+        //     'description' => 'Here is where you can register web routes for your application. These
+        //     | routes are loaded by the RouteServiceProvider within a group which
+        //     | contains the "web" middleware group. Now create something great!'
+        // ]);
+        Listing::factory(10)->create();
     }
 }
